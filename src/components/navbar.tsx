dfd7e9ece7fbdc344d488ba836b3fa20fddaeb1b
zhugezifang <xiaoqiu2017wy@163.com>
2025-01-10 01:56:13 +0000

    feat:menu

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -23,9 +23,12 @@ export default async function Navbar({
         <nav className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
           <div className="flex-1 md:flex-none">
             <div className="hidden items-center space-x-4 md:flex">
-              <Link href={`/en/multiple-line-graph-maker`} className="text-base font-medium transition-colors hover:text-primary">
-                {dict.nav.menu1}
+            {dict.nav.menu.map((menu) => (
+              <Link href={`/${menu.href}`} className="text-base font-medium transition-colors hover:text-primary">
+              {menu.name}
               </Link>
+            ))}
+              
               {/*
               <Link href={`/${lang}/blog`} className="text-base font-medium transition-colors hover:text-primary">
                 {dict.nav.blog}
