90e2e5f5c6b823b7e0629ec8a8563a5871d4d089
zhugezifang <xiaoqiu2017wy@163.com>
2025-01-16 10:29:04 +0000

    feat:bar

--- src/components/LineGraph.tsx
+++ src/components/LineGraph.tsx
@@ -8,14 +8,8 @@ export function LineGraph({
   
   return (
     <>
-      <Script src="https://code.jquery.com/jquery-3.6.0.min.js" strategy="beforeInteractive"/>
-      <Script src="https://www.xhs-download.online/static/js/tailwindcss.js" strategy="beforeInteractive"/>
-      <Script src="https://cdn.jsdelivr.net/npm/chart.js" strategy="beforeInteractive"/>
 
-      <Script src="/js/SingleLineGraph.js" strategy="afterInteractive"
-      />
-
-      <iframe id="game" src="/html/Line-Graph-Maker.html" allow="autoplay"  style={{ top: '0px', left: '0px',width: '100%', height: '800px'}}></iframe>
+      <iframe src="/html/Line-Graph-Maker.html" allow="autoplay"  style={{ top: '0px', left: '0px',width: '100%', height: '1250px'}}></iframe>
 
       
       
