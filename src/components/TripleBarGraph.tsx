4952582274be4477009696c42bff1eaa34c95b5d
zhugezifang <xiaoqiu2017wy@163.com>
2025-01-17 12:49:49 +0000

    feat:triple

--- src/components/TripleBarGraph.tsx
+++ src/components/TripleBarGraph.tsx
@@ -6,7 +6,7 @@ export function TripleBarGraph({
   
   return (
     <>
-          <iframe src="/html/Triple-Bar-Graph-Maker.html" allow="autoplay"  style={{ top: '0px', left: '0px',width: '100%', height: '1250px'}}></iframe>
+          <iframe src="/html/Triple-Bar-Graph-Maker.html" allow="autoplay"  style={{ top: '0px', left: '0px',width: '100%', height: '1450px'}}></iframe>
     </>
     
   )
