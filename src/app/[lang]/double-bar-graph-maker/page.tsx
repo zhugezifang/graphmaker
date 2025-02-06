95c84f04ae4427a33076ab57e8453c00f16d203a
zhugezifang <xiaoqiu2017wy@163.com>
2025-02-06 09:48:14 +0000

    feat:random

--- src/app/[lang]/double-bar-graph-maker/page.tsx
+++ src/app/[lang]/double-bar-graph-maker/page.tsx
@@ -12,27 +12,6 @@ export const runtime = 'edge'
 export default async function HandPage({ params: { lang } }: { params: { lang: Locale } }) {
   //const dict = await getDictionary(lang)
 
-  const dict = await getDictionary(lang);
-  // Bug 修复：显式声明 result 数组的类型
-  const result: { name:string, href: string }[] = [];
-  dict.nav.menu.forEach((menu) => {
-    if (menu.href === 'double-bar-graph-maker') {
-      return;
-    } else {
-      result.push(menu);
-    }
-  });
-
-  // 从result里面随机取三个
-  const randomThree = new Set();
-  while (randomThree.size < 3) {
-    const randomIndex = Math.floor(Math.random() * result.length);
-    randomThree.add(result[randomIndex]);
-  }
-
-  // 将 Set 转换为数组
-  const randomThreeArray:any = Array.from(randomThree);
-
   const post = await getPost('double-bar-graph-maker', lang) as unknown as { 
     title: string; 
     desc: string;
@@ -59,25 +38,7 @@ export default async function HandPage({ params: { lang } }: { params: { lang: L
       </article>
     </section>
 
-    <div className="bg-white py-2 my-6 px-4 mx-auto max-w-5xl sm:px-6 lg:px-8">
-
-        <div className="container mx-auto px-6 md:px-12 my-6 text-center">
-              <span className="text-4xl font-bold text-gray-800 mb-4">{dict.nav.more}</span>
-        </div>
-
-        <div className="grid grid-cols-3 gap-4 overflow-x-auto">
-
-            {randomThreeArray.map((menu: { name: string, href: string }) => (
-              <Link href={`/${lang}/${menu.href}`} className="text-base font-medium transition-colors hover:text-primary">
-                <div className="p-2">
-                  <p className="text-gray-600">{menu.name}</p>
-                </div>
-              </Link>
-            ))}
-
-        </div>
-
-    </div>
+    
 
     {/**/}
     </>
