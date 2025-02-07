384b3ebf89e8869dafa866c3d84af3e77948ec78
zhugezifang <xiaoqiu2017wy@163.com>
2025-02-07 01:56:04 +0000

    feat:quadruple

--- src/app/[lang]/quadruple-bar-graph-maker/page.tsx
+++ src/app/[lang]/quadruple-bar-graph-maker/page.tsx
@@ -51,8 +51,6 @@ export default async function HandPage({ params: { lang } }: { params: { lang: L
     </main>
 
     <iframe src="/html/Quadruple-Bar-Graph-Maker.html" allow="autoplay"  style={{ top: '0px', left: '0px',width: '100%', height: '1350px'}}></iframe>
-
-    <TripleBarGraph />
     
     <section className="bg-white py-2 my-10 px-4 mx-auto max-w-5xl sm:px-6 lg:px-8">
       <article className="prose prose-gray dark:prose-invert mx-auto">
