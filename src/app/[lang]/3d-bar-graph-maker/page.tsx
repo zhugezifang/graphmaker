23bbbfb6dc86871ecfcde224e469ce526bce1379
zhugezifang <xiaoqiu2017wy@163.com>
2025-03-25 13:02:41 +0000

    feat:cn

--- src/app/[lang]/3d-bar-graph-maker/page.tsx
+++ src/app/[lang]/3d-bar-graph-maker/page.tsx
@@ -48,7 +48,7 @@ export default async function HandPage({ params: { lang } }: { params: { lang: L
       
     </main>
 
-    <iframe src="/html/3D-Bar-Chart-Maker.html" allow="autoplay"  style={{ top: '0px', left: '0px',width: '100%', height: '1200px'}}></iframe>
+    <iframe src={lang === 'en' ? '/html/3D-Bar-Chart-Maker.html' : '/html/3D-Bar-Chart-Maker-CN.html'} allow="autoplay"  style={{ top: '0px', left: '0px',width: '100%', height: '1200px'}}></iframe>
     
     <section className="bg-white py-2 my-10 px-4 mx-auto max-w-5xl sm:px-6 lg:px-8">
       <article className="prose prose-gray dark:prose-invert mx-auto">
