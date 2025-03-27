551f7caf79b89c8345504f5342b2d4ee5533d3ef
zhugezifang <xiaoqiu2017wy@163.com>
2025-03-27 13:08:40 +0000

    feat:menu

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -28,12 +28,44 @@ export function MobileNav({ lang, dict }: MobileNavProps) {
       >
         <div className="absolute inset-0 bg-gradient-to-b from-primary/10 via-background/5 to-transparent pointer-events-none" />
         <nav className="relative flex flex-col space-y-4 mt-8">
-            {dict.nav.menu.map((menu: { href: string; name: string }) => (
-              <Link href={`/${lang}/${menu.href}`}  className="text-base font-medium transition-colors hover:text-primary"
-              >
-              {menu.name}
-              </Link>
-            ))}
+
+            <div className="relative group">
+              <button className="flex items-center text-base font-medium transition-colors hover:text-primary">
+                {dict.nav.title0}
+                <svg className="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path>
+                </svg>
+              </button>
+              <div className="absolute left-0 z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 group-hover:block">
+                <ul className="py-2 text-sm text-gray-700">
+                  {dict.nav.menu.map((menu: { href: string; name: string }) => (
+                    <li>
+                    <Link href={`/${lang}/${menu.href}`} className="text-base font-medium transition-colors hover:text-primary">{menu.name}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            <div className="relative group">
+              <button className="flex items-center text-base font-medium transition-colors hover:text-primary">
+                {dict.nav.title1}
+                <svg className="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path>
+                </svg>
+              </button>
+              <div className="absolute left-0 z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 group-hover:block">
+                <ul className="py-2 text-sm text-gray-700">
+                  {dict.nav.menu1.map((menu: { href: string; name: string }) => (
+                    <li>
+                    <Link href={`/${lang}/${menu.href}`} className="text-base font-medium transition-colors hover:text-primary">{menu.name}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          
+            
           {/*
           <Link 
               href={`/en/multiple-line-graph-maker`} 
